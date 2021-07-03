c9f80ede0db8e1b48484f4f83f09092347c2180d
Lo-Dyih-Yuaan <lzy1999891@163.com>
2021-07-03 13:55:57 +0800

    Update word.rs

--- src/word.rs
+++ src/word.rs
@@ -379,9 +379,7 @@ pub struct Word512(u128, u128, u128, u128);
 impl From<&str> for Word512 {
 	fn from(s: &str) -> Self {
 		let s = s.to_ascii_lowercase();
-		if !s.starts_with("0x") {
-			panic!();
-		}
+		aseert!(s.starts_with("0x"));
 		let s = &s[2..];
 		let n0 = u128::from_str_radix(&s[s.len()-32..s.len()], 16).unwrap();
 		let n1 = u128::from_str_radix(&s[s.len()-64..s.len()-32], 16).unwrap();
