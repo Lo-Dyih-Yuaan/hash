13770eea919bbf36ae8da914103bccbc45f6e6e1
Lo-Dyih-Yuaan <lzy1999891@163.com>
2021-07-23 23:06:51 +0800

    Add Whirlpool-0 and Whirlpool-T

--- src/whirlpool.rs
+++ src/whirlpool.rs
@@ -38,102 +38,128 @@ const S: [u8; 256] = [
 	0x2A, 0xBB, 0xc1, 0x53, 0xdc, 0x0B, 0x9d, 0x6c, 0x31, 0x74, 0xF6, 0x46, 0xAc, 0x89, 0x14, 0xE1,
 	0x16, 0x3A, 0x69, 0x09, 0x70, 0xB6, 0xd0, 0xEd, 0xcc, 0x42, 0x98, 0xA4, 0x28, 0x5c, 0xF8, 0x86
 ];
-fn gamma(n: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
-	let mut temp: [[u8; 8]; 8] = [[0; 8]; 8];
-	for i in 0..8 {
-		for j in 0..8 {
-			temp[i][j] = S[n[i][j] as usize];
-		}
-	}
-	temp
-}
-fn pi(n: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
-	let mut temp: [[u8; 8]; 8] = [[0; 8]; 8];
-	for i in 0..8 {
-		for j in 0..8 {
-			temp[i][j] = n[(i+8-j)%8][j];
-		}
-	}
-	temp
-}
-fn theta(n: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
-	fn mul(a: u8, b: u8) -> u8 {
-		let (mut a, mut b) = (a, b);
-		let mut p = 0u8;
-		while a != 0 && b != 0 {
-			if b & 1 != 0 {
-				p ^= a;
+const S0: [u8; 256] = [
+	0x68, 0xD0, 0xEB, 0x2B, 0x48, 0x9D, 0x6A, 0xE4, 0xE3, 0xA3, 0x56, 0x81, 0x7D, 0xF1, 0x85, 0x9E,
+	0x2C, 0x8E, 0x78, 0xCA, 0x17, 0xA9, 0x61, 0xD5, 0x5D, 0x0B, 0x8C, 0x3C, 0x77, 0x51, 0x22, 0x42,
+	0x3F, 0x54, 0x41, 0x80, 0xCC, 0x86, 0xB3, 0x18, 0x2E, 0x57, 0x06, 0x62, 0xF4, 0x36, 0xD1, 0x6B,
+	0x1B, 0x65, 0x75, 0x10, 0xDA, 0x49, 0x26, 0xF9, 0xCB, 0x66, 0xE7, 0xBA, 0xAE, 0x50, 0x52, 0xAB,
+	0x05, 0xF0, 0x0D, 0x73, 0x3B, 0x04, 0x20, 0xFE, 0xDD, 0xF5, 0xB4, 0x5F, 0x0A, 0xB5, 0xC0, 0xA0,
+	0x71, 0xA5, 0x2D, 0x60, 0x72, 0x93, 0x39, 0x08, 0x83, 0x21, 0x5C, 0x87, 0xB1, 0xE0, 0x00, 0xC3,
+	0x12, 0x91, 0x8A, 0x02, 0x1C, 0xE6, 0x45, 0xC2, 0xC4, 0xFD, 0xBF, 0x44, 0xA1, 0x4C, 0x33, 0xC5,
+	0x84, 0x23, 0x7C, 0xB0, 0x25, 0x15, 0x35, 0x69, 0xFF, 0x94, 0x4D, 0x70, 0xA2, 0xAF, 0xCD, 0xD6,
+	0x6C, 0xB7, 0xF8, 0x09, 0xF3, 0x67, 0xA4, 0xEA, 0xEC, 0xB6, 0xD4, 0xD2, 0x14, 0x1E, 0xE1, 0x24,
+	0x38, 0xC6, 0xDB, 0x4B, 0x7A, 0x3A, 0xDE, 0x5E, 0xDF, 0x95, 0xFC, 0xAA, 0xD7, 0xCE, 0x07, 0x0F,
+	0x3D, 0x58, 0x9A, 0x98, 0x9C, 0xF2, 0xA7, 0x11, 0x7E, 0x8B, 0x43, 0x03, 0xE2, 0xDC, 0xE5, 0xB2,
+	0x4E, 0xC7, 0x6D, 0xE9, 0x27, 0x40, 0xD8, 0x37, 0x92, 0x8F, 0x01, 0x1D, 0x53, 0x3E, 0x59, 0xC1,
+	0x4F, 0x32, 0x16, 0xFA, 0x74, 0xFB, 0x63, 0x9F, 0x34, 0x1A, 0x2A, 0x5A, 0x8D, 0xC9, 0xCF, 0xF6,
+	0x90, 0x28, 0x88, 0x9B, 0x31, 0x0E, 0xBD, 0x4A, 0xE8, 0x96, 0xA6, 0x0C, 0xC8, 0x79, 0xBC, 0xBE,
+	0xEF, 0x6E, 0x46, 0x97, 0x5B, 0xED, 0x19, 0xD9, 0xAC, 0x99, 0xA8, 0x29, 0x64, 0x1F, 0xAD, 0x55,
+	0x13, 0xBB, 0xF7, 0x6F, 0xB9, 0x47, 0x2F, 0xEE, 0xB8, 0x7B, 0x89, 0x30, 0xD3, 0x7F, 0x76, 0x82
+];
+
+const L: [u8; 8] = [1, 1, 4, 1, 8, 5, 2, 9];
+const L0: [u8; 8] = [1, 1, 3, 1, 5, 8, 9, 5];
+
+macro_rules! generate_hash {
+	{$f:ident, $s:expr, $l:expr} => {
+		pub fn $f(message: &[u8]) -> [u8; 64] {
+			fn gamma(n: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
+				let mut temp: [[u8; 8]; 8] = [[0; 8]; 8];
+				for i in 0..8 {
+					for j in 0..8 {
+						temp[i][j] = $s[n[i][j] as usize];
+					}
+				}
+				temp
 			}
-			a = (a << 1) ^ if a > 0x7f {0x1d} else {0};
-			b >>= 1;
-		}
-		p
-	}
-	const C: [[u8; 8]; 8] = [
-		[1, 1, 4, 1, 8, 5, 2, 9],
-		[9, 1, 1, 4, 1, 8, 5, 2],
-		[2, 9, 1, 1, 4, 1, 8, 5],
-		[5, 2, 9, 1, 1, 4, 1, 8],
-		[8, 5, 2, 9, 1, 1, 4, 1],
-		[1, 8, 5, 2, 9, 1, 1, 4],
-		[4, 1, 8, 5, 2, 9, 1, 1],
-		[1, 4, 1, 8, 5, 2, 9, 1]
-	];
-	let mut temp: [[u8; 8]; 8] = [[0; 8]; 8];
-	for i in 0..8 {
-		for j in 0..8 {
-			for k in 0..8 {
-				temp[i][j] = temp[i][j]^mul(n[i][k], C[k][j]);
+			fn pi(n: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
+				let mut temp: [[u8; 8]; 8] = [[0; 8]; 8];
+				for i in 0..8 {
+					for j in 0..8 {
+						temp[i][j] = n[(i+8-j)%8][j];
+					}
+				}
+				temp
 			}
+			fn theta(n: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
+				fn mul(a: u8, b: u8) -> u8 {
+					let (mut a, mut b) = (a, b);
+					let mut p = 0u8;
+					while a != 0 && b != 0 {
+						if b & 1 != 0 {
+							p ^= a;
+						}
+						a = (a << 1) ^ if a > 0x7f {0x1d} else {0};
+						b >>= 1;
+					}
+					p
+				}
+				const C: [[u8; 8]; 8] = [
+					[$l[0], $l[1], $l[2], $l[3], $l[4], $l[5], $l[6], $l[7]],
+					[$l[7], $l[0], $l[1], $l[2], $l[3], $l[4], $l[5], $l[6]],
+					[$l[6], $l[7], $l[0], $l[1], $l[2], $l[3], $l[4], $l[5]],
+					[$l[5], $l[6], $l[7], $l[0], $l[1], $l[2], $l[3], $l[4]],
+					[$l[4], $l[5], $l[6], $l[7], $l[0], $l[1], $l[2], $l[3]],
+					[$l[3], $l[4], $l[5], $l[6], $l[7], $l[0], $l[1], $l[2]],
+					[$l[2], $l[3], $l[4], $l[5], $l[6], $l[7], $l[0], $l[1]],
+					[$l[1], $l[2], $l[3], $l[4], $l[5], $l[6], $l[7], $l[0]],
+				];
+				let mut temp: [[u8; 8]; 8] = [[0; 8]; 8];
+				for i in 0..8 {
+					for j in 0..8 {
+						for k in 0..8 {
+							temp[i][j] = temp[i][j]^mul(n[i][k], C[k][j]);
+						}
+					}
+				}
+				temp
+			}
+			fn sigma(k: [[u8; 8]; 8], a: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
+				let mut temp: [[u8; 8]; 8] = [[0; 8]; 8];
+				for i in 0..8 {
+					for j in 0..8 {
+						temp[i][j] = k[i][j] ^ a[i][j];
+					}
+				}
+				temp
+			}
+			fn rho(k: [[u8; 8]; 8], a: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
+				sigma(k, theta(pi(gamma(a))))
+			}
+			fn w(k: [[u8; 8]; 8], a: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
+				let mut temp: [[u8; 8]; 8] = sigma(k, a);
+				let mut k = k;
+				for r in 0..10 {
+					let mut c: [[u8; 8]; 8] = [[0; 8]; 8];
+					for i in 0..8 {
+						c[0][i] = $s[8*r+i];
+					}
+					k = rho(c, k);
+					temp = rho(k, temp);
+				}
+				temp
+			}
+			let len = Word256::from_bits(Bits::from(message.len()) << 3);
+			//补位
+			let padding = 64 - (message.len() + 32)%64;
+			let mut m: Vec<u8> = Vec::with_capacity(message.len() + padding);
+			m.extend_from_slice(message);
+			m.push(0x80u8);
+			m.extend(&vec![0; padding-1]);
+			//增加长度
+			m.extend_from_slice(&len.to_be_bytes());
+			//
+			let m: &[Word512] = &Word512::slice_from_be_bytes(&m);
+			//
+			let mut h: Word512 = Word512::from(0);
+			for m in m {
+				h = amu(w(mu(h), mu(*m))) ^ h ^ *m;
+			}
+			//输出
+			h.to_be_bytes()
 		}
 	}
-	temp
 }
-fn sigma(k: [[u8; 8]; 8], a: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
-	let mut temp: [[u8; 8]; 8] = [[0; 8]; 8];
-	for i in 0..8 {
-		for j in 0..8 {
-			temp[i][j] = k[i][j] ^ a[i][j];
-		}
-	}
-	temp
-}
-fn rho(k: [[u8; 8]; 8], a: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
-	sigma(k, theta(pi(gamma(a))))
-}
-fn w(k: [[u8; 8]; 8], a: [[u8; 8]; 8]) -> [[u8; 8]; 8] {
-	let mut temp: [[u8; 8]; 8] = sigma(k, a);
-	let mut k = k;
-	for r in 0..10 {
-		let mut c: [[u8; 8]; 8] = [[0; 8]; 8];
-		for i in 0..8 {
-			c[0][i] = S[8*r+i];
-		}
-		k = rho(c, k);
-		temp = rho(k, temp);
-	}
-	temp
-}
-
-
-
-pub fn whirlpool(message: &[u8]) -> [u8; 64] {
-	let len = Word256::from_bits(Bits::from(message.len()) << 3);
-	//补位
-	let padding = 64 - (message.len() + 32)%64;
-	let mut m: Vec<u8> = Vec::with_capacity(message.len() + padding);
-	m.extend_from_slice(message);
-	m.push(0x80u8);
-	m.extend(&vec![0; padding-1]);
-	//增加长度
-	m.extend_from_slice(&len.to_be_bytes());
-	//
-	let m: &[Word512] = &Word512::slice_from_be_bytes(&m);
-	//
-	let mut h: Word512 = Word512::from(0);
-	for m in m {
-		h = amu(w(mu(h), mu(*m))) ^ h ^ *m;
-	}
-	//输出
-	h.to_be_bytes()
-}
+generate_hash!{whirlpool, S, L}
+generate_hash!{whirlpool_t, S, L0}
+generate_hash!{whirlpool_0, S0, L0}
